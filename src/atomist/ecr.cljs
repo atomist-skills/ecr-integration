201c9a6db7802a1cca89e3ba1dd904e665bf7689
Jim Clark <jim@atomist.com>
2021-07-02 11:59:50 -0700

    Add the token operation and verify that both strategies work

--- src/atomist/ecr.cljs
+++ src/atomist/ecr.cljs
@@ -33,20 +33,21 @@
    [atomist.docker :as docker]
    [cljs-node-io.core :as io]
    [cljs.pprint :refer [pprint]]
+   ["@aws-sdk/client-eventbridge" :as eventbridge]
    ["@aws-sdk/client-ecr" :as ecr-service]
    ["@aws-sdk/client-sts" :as sts-service]
    ["aws-sdk" :as aws-sdk]
-   ["tmp" :as tmp]
    [atomist.json :as json]))
 
-(enable-console-print!)
 (set! *warn-on-infer* false)
 
 (def third-party-account-id (.. js/process -env -ECR_ACCOUNT_ID))
 (def third-party-arn "arn:aws:iam::111664719423:role/atomist-ecr-integration")
 (def third-party-external-id "atomist")
+(def third-party-secret-key (.. js/process -env -ECR_SECRET_ACCESS_KEY))
+(def third-party-access-key-id (.. js/process -env -ECR_ACCESS_KEY_ID))
 (def atomist-account-id (.. js/process -env -ASSUME_ROLE_ACCOUNT_ID))
-(def atomist-account-key (.. js/process -env -ASSUME_ROLE_ACCESS_KEY_ID))
+(def atomist-access-key-id (.. js/process -env -ASSUME_ROLE_ACCESS_KEY_ID))
 (def atomist-secret-key (.. js/process -env -ASSUME_ROLE_SECRET_ACCESS_KEY))
 (enable-console-print!)
 
@@ -88,109 +89,133 @@
       (-> (.-repositories data) (js->clj :keywordize-keys true)))
     (partial wrap-error-in-exception "failed to run DescribeRepositoriesCommand")))
 
-(defn assume-role-aws-sdk-service [{:keys [arn external-id]} service-constructor operation]
+(defn get-authorization-token-command
+  [ecr-client]
+  (from-promise
+   (.send ecr-client (new (.-GetAuthorizationTokenCommand ecr-service) #js {}))
+   (fn [data]
+     (-> data
+         (. -authorizationData)
+         (aget 0)
+         (. -authorizationToken)))
+   (partial wrap-error-in-exception "failed to create token")))
+
+(defn call-aws-sdk-service
+  "call aws-sdk v3 operations 
+     - may use STS to assume role if there is an arn present.  Otherwise, default to use creds without STS"
+  [{:keys [arn external-id access-key-id secret-access-key region]}
+   service-constructor
+   operation]
   (let [f (io/file "atomist-config.json")]
     (io/spit f (->
-                {:region "us-east-1"
-                 :accessKeyId atomist-account-key
-                 :secretAccessKey atomist-secret-key
+                {:region region
+                 :accessKeyId access-key-id
+                 :secretAccessKey secret-access-key
                  :httpOptions {:timeout 3 :connectTimeout 5000}
                  :maxRetries 3}
                 (json/->str)))
     (.loadFromPath (.. aws-sdk -config) (.getPath f))
-    (let [client (new (.-STSClient sts-service) (. aws-sdk -config))]
-      (from-promise
-       (.send client (new (.-AssumeRoleCommand sts-service) #js {:ExternalId external-id
-                                                                 :RoleArn arn
-                                                                 :RoleSessionName "atomist"}))
-     (with-meta
-         (fn [data]
-           (operation
-            (new service-constructor 
-                 #js {:credentials #js {:accessKeyId (.. data -Credentials -AccessKeyId)
-                                        :secretAccessKey (.. data -Credentials -SecretAccessKey)
-                                        :sessionToken (.. data -Credentials -SessionToken)}})))
-         {:async true})
-       (partial wrap-error-in-exception "failed to create token")))))
+    (io/delete-file f)
+    (let [client (new (.-STS sts-service) (. aws-sdk -config))]
+      (if (and arn external-id)
+        (from-promise
+         (.send client
+                (new (.-AssumeRoleCommand sts-service)
+                     #js {:ExternalId external-id
+                          :RoleArn arn
+                          :RoleSessionName "atomist"
+                          :credentials #js {:accessKeyId access-key-id
+                                            :secretAccessKey secret-access-key}}))
+         (with-meta
+           (fn [data]
+             (operation
+              (new service-constructor
+                   #js {:credentials #js {:accessKeyId (.. data -Credentials -AccessKeyId)
+                                          :secretAccessKey (.. data -Credentials -SecretAccessKey)
+                                          :sessionToken (.. data -Credentials -SessionToken)}})))
+           {:async true})
+         (partial wrap-error-in-exception "failed to create token"))
+        (operation (new service-constructor
+                        #js {:credentials #js {:accessKeyId access-key-id
+                                               :secretAccessKey secret-access-key}}))))))
 
 (comment
-  ;; we are querying across accounts here
-  (go (pprint (<! (assume-role-aws-sdk-service 
-                    {:arn third-party-arn
-                     :external-id "atomist"}
-                    (.-ECR ecr-service)
-                    list-repositories))))
+  ;; we are querying across accounts here (using role arn and sts)
+  (go (pprint (<! (call-aws-sdk-service
+                   {:region "us-east-1"
+                    :access-key-id atomist-access-key-id
+                    :secret-access-key atomist-secret-key
+                    :arn third-party-arn
+                    :external-id "atomist"}
+                   (.-ECR ecr-service)
+                   list-repositories))))
+  ;; use creds to get an auth token
+  (go (pprint (<! (call-aws-sdk-service
+                   {:region "us-east-1"
+                    :access-key-id third-party-access-key-id
+                    :secret-access-key third-party-secret-key}
+                   (.-ECR ecr-service)
+                   get-authorization-token-command))))
+  ;; use role arn and sts to get an auth token
+  (go (pprint (<! (call-aws-sdk-service
+                   {:region "us-east-1"
+                    :access-key-id atomist-access-key-id
+                    :secret-access-key atomist-secret-key
+                    :arn third-party-arn
+                    :external-id third-party-external-id }
+                   (.-ECR ecr-service)
+                   get-authorization-token-command))))
   )
 
 (defn account-host
   [account-id region]
   (gstring/format "%s.dkr.ecr.%s.amazonaws.com" account-id region))
 
-(defn- empty-tmp-dir
-  "we can only write AWS SDK config to a tmp dir on GCF"
-  []
-  (let [c (chan)]
-    (.dir tmp
-          (clj->js {:keep false :prefix (str "atm-" (. js/process -pid))})
-          (fn [err path]
-            (go
-              (if err
-                (>! c (ex-info "could not create tmp dir" {:err err}))
-                (>! c path))
-              (close! c))))
-    c))
-
-(defn with-close [f]
-  (let [c (chan)]
-    (go
-      (<! c)
-      (try
-        (io/delete-file f)
-        (catch :default ex
-          (log/warn "unable to delete " f))))
-    c))
-
 (defn ecr-auth
-  [{:keys [region access-key-id secret-access-key]}]
+  "get an ecr authorization token"
+  [{:keys [region] :as params}]
   (go-safe
    (log/infof "Authenticating GCR in region %s" region)
-   (let [f (io/file (<? (empty-tmp-dir)) "config.json")]
-     (io/spit f (-> {:region region
-                     :accessKeyId access-key-id
-                     :secretAccessKey secret-access-key
-                     :httpOptions {:timeout 5000 :connectTimeout 5000}
-                     :maxRetries 3}
-                    (json/->str)))
-     (.loadFromPath (.. aws-sdk -config) (.getPath f))
-     (let [token-chan (chan)
-           client (new (.-ECRClient ecr-service) (. aws-sdk -config))]
-       ;; write to in-memory file-system for GCF and remove when complete
-
-     ;; Send AWS GetAuthorizationTokenCommand
-       (.catch
-        (.then
-         (.send client (new (.-GetAuthorizationTokenCommand ecr-service) #js {}))
-         (fn [data] (go
-                      (>! (with-close f) :close)
-                      (>! token-chan (-> data
-                                         (. -authorizationData)
-                                         (aget 0)
-                                         (. -authorizationToken))))))
-        (fn [err] (go
-                    (log/warn "err " err)
-                    (>! (with-close f) :close)
-                    (>! token-chan (ex-info "failed to create token" {:err err})))))
-       {:access-token (<? token-chan)}))))
+   {:access-token
+    (<? (call-aws-sdk-service
+         params
+         (.-ECR ecr-service)
+         get-authorization-token-command))}))
 
 (defn get-labelled-manifests
   "log error or return labels"
-  [{:keys [account-id region access-key-id secret-access-key]} repository tag-or-digest]
+  [{:keys [account-id region access-key-id] :as params} repository tag-or-digest]
   (log/infof "get-image-info:  %s@%s/%s" region access-key-id tag-or-digest)
   (go-safe
-   (let [auth-context (<? (ecr-auth {:region region
-                                     :secret-access-key secret-access-key
-                                     :access-key-id access-key-id}))]
+   (let [auth-context (<? (ecr-auth params))]
      (<? (docker/get-labelled-manifests
           (account-host account-id region)
           (:access-token auth-context) repository tag-or-digest)))))
 
+(comment
+  ;; first test with the third party creds
+  (go
+    (pprint
+     (<!
+      (get-labelled-manifests
+       {:account-id third-party-account-id
+        :region "us-east-1"
+        :access-key-id third-party-access-key-id
+        :secret-access-key third-party-secret-key}
+       "pin-test"
+       "sha256:5a703f57de904d1b189df40206458a6e3d9e9526b7bfd94dab8519e1c51b7a0c"))))
+
+  ;; next, test with the atomist creds, and a third party trusted arn
+  (go
+    (pprint
+     (<!
+      (get-labelled-manifests
+       {:account-id third-party-account-id
+        :region "us-east-1"
+        :access-key-id atomist-access-key-id
+        :secret-access-key atomist-secret-key
+        :external-id "atomist"
+        :arn third-party-arn}
+       "pin-test"
+       "sha256:5a703f57de904d1b189df40206458a6e3d9e9526b7bfd94dab8519e1c51b7a0c"))))
+  )
